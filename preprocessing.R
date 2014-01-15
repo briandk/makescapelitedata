c0536891f488525bdf5f17331eab4fc0bad42e66
Brian A. Danielak <brian@briandk.com>
2014-01-15 14:40:42 -0600

    Add function to standardize column names

--- preprocessing.R
+++ preprocessing.R
@@ -1,6 +1,22 @@
+# Required libraries
+library(ggplot2)
+library(plyr)
+
 setwd("~/Dropbox/IDC 2014/log data/")
 
 set1 <- read.csv(file="2013.12.10-01-actions.log.csv",
                  header=TRUE,
                  stringsAsFactors=FALSE)
-View(set1)
+
+renameColumnHeaders <- function(input) {
+  return(
+    rename(input, c("time_stamp"              = "timestamp",
+                    "sec_since_session_start" = "secondsSinceSessionStart",
+                    "data"                    = "logData",
+                    "screen_position"         = "screenPosition",
+                    "X__class__"              = "activityType")
+    )
+  )
+}
+
+names(renameColumnHeaders(set1))
